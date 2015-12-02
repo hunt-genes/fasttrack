18c7b9b2819bd8987396dfc90861f7b538d67513
Sigurd Gartmann <sigurd.gartmann@ntnu.no>
2015-12-02 11:26:25 +0100

    Add redirect on / until we have a nice frontpage or do something fancy.
    Closes #11

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,17 @@
 var ReactRouter = require("react-router");
 //import {Route, IndexRoute} from "react-router";
 var Route = ReactRouter.Route;
+var Redirect = ReactRouter.Redirect;
 var React = require("react");
 var App = require("./components/App");
 var Search = require("./components/Search");
 
 var routes = (
-    <Route path="/search" component={App}>
-        <Route path=":q" ignoreScrollBehaviour />
+    <Route>
+        <Route path="/search" component={App}>
+            <Route path=":q" ignoreScrollBehaviour />
+        </Route>
+        <Redirect from="/" to="/search" />
     </Route>
 );
 
