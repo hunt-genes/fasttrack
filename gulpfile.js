e55d5c2dff6e8f301d8e1271c61efeba1b884225
Sigurd Gartmann <sigurd.gartmann@ntnu.no>
2015-12-04 11:26:47 +0100

    Set up production config for webpack. And combine build-dev and watch.

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,39 @@ var gulp  = require('gulp'),
     sass = require("gulp-sass"),
     webpack = require("webpack");
 
-gulp.task('default', ['watch']);
-gulp.task("build", ["sass", "icons", "images", "webpack"]);
+var config = {
+    production: !!gutil.env.production
+};
+
+var webpackConfig = {
+    devtool: "cheap-module-source-map",
+    context: __dirname + "/src",
+    entry: "./client.js",
+    plugins: [],
+    output: {
+        filename: "client.js",
+        path: __dirname + "/dist",
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loaders: ["babel-loader"],
+            }
+        ],
+    }
+};
+
+
+gulp.task("default", ["build-dev"]);
+gulp.task("build", ["sass", "icons", "images", "webpack:build"]);
+gulp.task("build-dev", ["webpack:build-dev", "sass", "icons", "images"], function() {
+  gulp.watch("src/**/*.js", ["webpack:build-dev"]);
+  gulp.watch("src/scss/*.scss", ["sass"]);
+  gulp.watch("src/images/*.scss", ["images"]);
+});
+
 
 gulp.task('sass', function() {
     return gulp.src('src/scss/stylesheet.scss')
@@ -12,30 +43,32 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task("webpack", function(callback) {
-    // run webpack
-    webpack({
-        devtool: "cheap-module-source-map",
-        context: __dirname + "/src",
-        entry: "./client.js",
+gulp.task("webpack:build-dev", function(callback) {
+    webpack(webpackConfig, function(err, stats) {
+        if(err) throw new gutil.PluginError("webpack:build-dev", err);
+        gutil.log("[webpack:build-dev]", stats.toString({
+            colors: true
+        }));
+        callback();
+    });
+});
 
-        output: {
-            filename: "client.js",
-            path: __dirname + "/dist",
-        },
-        module: {
-            loaders: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    loaders: ["babel-loader"],
-                }
-            ],
-        }
-    }, function(err, stats) {
-        if(err) throw new gutil.PluginError("webpack", err);
-        gutil.log("[webpack]", stats.toString({
-            // output options
+gulp.task("webpack:build", function(callback) {
+    var prodConfig = Object.create(webpackConfig);
+    prodConfig.plugins = prodConfig.plugins.concat(
+        new webpack.DefinePlugin({
+            "process.env": {
+                // This has effect on the react lib size
+                "NODE_ENV": JSON.stringify("production")
+            }
+        }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin()
+    );
+    webpack(prodConfig, function(err, stats) {
+        if(err) throw new gutil.PluginError("webpack:build", err);
+        gutil.log("[webpack:build]", stats.toString({
+            colors: true
         }));
         callback();
     });
@@ -50,9 +83,3 @@ gulp.task("images", function () {
     return gulp.src("./src/images/**/*.*")
         .pipe(gulp.dest("./dist/img"));
 });
-
-gulp.task('watch', function() {
-  gulp.watch('src/**/*.js', ['webpack']);
-  gulp.watch('src/scss/*.scss', ['sass']);
-});
-
