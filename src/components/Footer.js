e6a1f9bbf4765f73fb91f514a7fbf4d93a7e8aa5
Sigurd Gartmann <sigurdga@sigurdga.no>
2016-01-23 16:03:14 +0100

    Using the new link to our group, english url and new english name.
    Close #36

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -41,7 +41,7 @@ class Footer extends React.Component {
                             <footer style={{ fontSize: 11, color: "#aaa", textAlign: "center", paddingBottom: 50 }}>
                                 GWAS data from <ExternalLink href="https://www.ebi.ac.uk/gwas/docs/downloads">NHGRI-EBI</ExternalLink><br />
                                 The usual warnings about providing the service AS-IS applies.<br />
-                                <ExternalLink href="http://www.ntnu.no/ism/epicenter/home">Human genetic epidemiology group (HGE)</ExternalLink>, <ExternalLink href="http://www.ntnu.edu/ism">Department of public health and general practice (ISM)</ExternalLink>, <ExternalLink href="http://www.ntnu.edu/">Norwegian university of science and technology (NTNU)</ExternalLink>
+                                <ExternalLink href="https://www.ntnu.edu/huntgenes">K.G. Jebsen Center for Genetic Epidemiology</ExternalLink>, <ExternalLink href="http://www.ntnu.edu/ism">Department of public health and general practice (ISM)</ExternalLink>, <ExternalLink href="http://www.ntnu.edu/">Norwegian university of science and technology (NTNU)</ExternalLink>
                                 <br />
                                 {statistics}
                             </footer>
