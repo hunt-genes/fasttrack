125f5d1e30885e2ed1b7cbb794058eff1b565d1a
Sigurd Gartmann <sigurd.gartmann@ntnu.no>
2016-02-17 14:50:47 +0100

    Create a component for result lines

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,11 @@ import GwasActions from "../actions/GwasActions";
 import GwasStore from "../stores/GwasStore";
 import { Input, Button, Table, Alert, Grid, Row, Col, Image } from "react-bootstrap";
 import { Link } from "react-router";
-import ExternalLink from "./ExternalLink";
 import Footer from "./Footer";
 import TraitList from "./TraitList";
-import ImputationResults from "./ImputationResults";
+import Result from "./Result";
 
 class App extends React.Component {
-
     static getStores() {
         return [GwasStore];
     }
@@ -64,19 +62,6 @@ class App extends React.Component {
         this.props.history.pushState(null, `/search/?hunt=${this.refs.hunt.getChecked()}`);
     }
 
-    rowclass(p) {
-        if (p > 0.00000005) {
-            return "uninteresting";
-        }
-    }
-    exp(number) {
-        if (number && !isNaN(number)) {
-            return number.toExponential();
-        }
-    }
-    getYear(datestring) {
-        return datestring.split("-").pop();
-    }
     renderResults() {
         if (!this.props.location.query.q) {
             return (
@@ -115,86 +100,7 @@ class App extends React.Component {
                     </thead>
                     <tbody>
                         {this.props.results.get("data").map(result =>
-                        <tr key={result.get("_id")} className={this.rowclass(result.get("P-VALUE"))}>
-                            <td>
-                                <div>
-                                    <Link to={`/search/?q=${result.get("SNP_ID_CURRENT")}`}>
-                                        {result.get("SNPS")}
-                                    </Link>
-                                </div>
-                            </td>
-                            <td>
-                                {result.get("hunt")}
-                                <ImputationResults imputed={result.get('imputed')} />
-                            </td>
-                            <td>
-                                <div>
-                                    {this.exp(result.get("P-VALUE")) || "0.0" }
-                                </div>
-                                <div>
-                                    {result.get("P-VALUE (TEXT)")}
-                                </div>
-                            </td>
-                            <td>
-                                <div>
-                                    <Link to={`/search/?q=${result.get("REGION")}`}>
-                                        {result.get("REGION")}
-                                    </Link>
-                                </div>
-                                <div>
-                                    <Link to={result.get("CHR_ID") ? `/search/?q=chr${result.get("CHR_ID")}:${result.get("CHR_POS")}` : ""}>
-                                        {result.get("CHR_ID") ? `chr${result.get("CHR_ID")}:${result.get("CHR_POS")}` : ""}
-                                    </Link>
-                                </div>
-                                <div title={result.get("CONTEXT")} style={{ maxWidth: "100px", overflow: "hidden", textOverflow: "ellipsis", whiteSpace: "nowrap" }}>
-                                    {result.get("CONTEXT")}
-                                </div>
-                            </td>
-                            <td>
-                                <ul>
-                                    {result.get("MAPPED_GENE").split(" - ").map(gene => {
-                                        return (
-                                            <li key={gene}>
-                                                <Link to={`/search/?q=${gene}`}>
-                                                    {gene}
-                                                </Link> <ExternalLink href={`http://www.genecards.org/cgi-bin/carddisp.pl?gene=${gene}`} />
-                                            </li>
-                                            );
-                                    })
-                                    }
-                                </ul>
-                            </td>
-                            <td>
-                                <ul>
-                                {result.get("MAPPED_TRAIT").split(", ").map(trait =>
-                                    <li key={trait}>
-                                        <Link to={`/search/?q=${trait}`}>
-                                            {trait}
-                                        </Link>
-                                    </li>
-                                )}
-                                </ul>
-                            </td>
-                            <td>
-                                <div>{result.get("OR or BETA")}</div>
-                                <div>{result.get("95% CI (TEXT)")}</div>
-                            </td>
-                            <td>
-                                <div>{this.getYear(result.get("DATE"))}</div>
-                                <div className="uninteresting">{result.get("DATE ADDED TO CATALOG")}</div>
-                            </td>
-                            <td>
-                                <Link to={`/search/?q=${result.get("FIRST AUTHOR")}`}>
-                                    {result.get("FIRST AUTHOR")}
-                                </Link>
-                            </td>
-                            <td>
-                                <Link to={`/search/?q=${result.get("PUBMEDID")}`}>
-                                    {result.get("PUBMEDID")}
-                                </Link> <ExternalLink href={`http://www.ncbi.nlm.nih.gov/pubmed/${result.get("PUBMEDID")}`} />
-                                <div>{result.get("JOURNAL")}</div>
-                            </td>
-                        </tr>
+                            <Result key={result.get("_id")} data={result} />
                         )}
                     </tbody>
                 </Table>
---
+++ src/components/Result.js
@@ -0,0 +1,110 @@
+import React from "react";
+import { Link } from "react-router";
+
+import ImputationResults from "./ImputationResults";
+import ExternalLink from "./ExternalLink";
+
+class Result extends React.Component {
+    rowclass(p) {
+        if (p > 0.00000005) {
+            return "uninteresting";
+        }
+    }
+    exp(number) {
+        if (number && !isNaN(number)) {
+            return number.toExponential();
+        }
+    }
+    getYear(datestring) {
+        return datestring.split("-").pop();
+    }
+
+    render() {
+        const result = this.props.data;
+        return (
+            <tr key={result.get("_id")} className={this.rowclass(result.get("P-VALUE"))}>
+                <td>
+                    <div>
+                        <Link to={`/search/?q=${result.get("SNP_ID_CURRENT")}`}>
+                            {result.get("SNPS")}
+                        </Link>
+                    </div>
+                </td>
+                <td>
+                    {result.get("hunt")}
+                    <ImputationResults imputed={result.get("imputed")} />
+                </td>
+                <td>
+                    <div>
+                        {this.exp(result.get("P-VALUE")) || "0.0" }
+                    </div>
+                    <div>
+                        {result.get("P-VALUE (TEXT)")}
+                    </div>
+                </td>
+                <td>
+                    <div>
+                        <Link to={`/search/?q=${result.get("REGION")}`}>
+                            {result.get("REGION")}
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to={result.get("CHR_ID") ? `/search/?q=chr${result.get("CHR_ID")}:${result.get("CHR_POS")}` : ""}>
+                            {result.get("CHR_ID") ? `chr${result.get("CHR_ID")}:${result.get("CHR_POS")}` : ""}
+                        </Link>
+                    </div>
+                    <div title={result.get("CONTEXT")} style={{ maxWidth: "100px", overflow: "hidden", textOverflow: "ellipsis", whiteSpace: "nowrap" }}>
+                        {result.get("CONTEXT")}
+                    </div>
+                </td>
+                <td>
+                    <ul>
+                        {result.get("MAPPED_GENE").split(" - ").map(gene =>
+                            <li key={gene}>
+                                <Link to={`/search/?q=${gene}`}>
+                                    {gene}
+                                </Link> <ExternalLink href={`http://www.genecards.org/cgi-bin/carddisp.pl?gene=${gene}`} />
+                            </li>
+                        )}
+                    </ul>
+                </td>
+                <td>
+                    <ul>
+                        {result.get("MAPPED_TRAIT").split(", ").map(trait =>
+                        <li key={trait}>
+                            <Link to={`/search/?q=${trait}`}>
+                                {trait}
+                            </Link>
+                        </li>
+                        )}
+                    </ul>
+                </td>
+                <td>
+                    <div>{result.get("OR or BETA")}</div>
+                    <div>{result.get("95% CI (TEXT)")}</div>
+                </td>
+                <td>
+                    <div>{this.getYear(result.get("DATE"))}</div>
+                    <div className="uninteresting">{result.get("DATE ADDED TO CATALOG")}</div>
+                </td>
+                <td>
+                    <Link to={`/search/?q=${result.get("FIRST AUTHOR")}`}>
+                        {result.get("FIRST AUTHOR")}
+                    </Link>
+                </td>
+                <td>
+                    <Link to={`/search/?q=${result.get("PUBMEDID")}`}>
+                        {result.get("PUBMEDID")}
+                    </Link> <ExternalLink href={`http://www.ncbi.nlm.nih.gov/pubmed/${result.get("PUBMEDID")}`} />
+                    <div>{result.get("JOURNAL")}</div>
+                </td>
+            </tr>
+        );
+    }
+}
+
+Result.propTypes = {
+    data: React.PropTypes.object,
+};
+
+export default Result;
