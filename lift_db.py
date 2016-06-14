0f6644c7a8cf010838c0cbbacf85bf12b445d3e0
Sigurd Gartmann <sigurd.gartmann@ntnu.no>
2016-06-14 14:52:53 +0200

    Add script to lift database to hg19, using new columns.

---
+++ lift_db.py
@@ -0,0 +1,26 @@
+from pymongo import MongoClient
+from pyliftover import LiftOver
+
+mongo_client = MongoClient()
+db = mongo_client.gwasc
+
+lo = LiftOver('hg38ToHg19.over.chain.gz')
+
+unmatched = 0
+matched = 0
+for r in db.gwas.find():
+    chrid = r['CHR_ID']
+    chrpos = r['CHR_POS']
+    if chrid and chrpos:
+        lifted = lo.convert_coordinate('chr%s' % chrid, chrpos - 1)
+        if lifted:
+            new_chrid = lifted[0][0].split('chr')[1]
+            new_chrpos = lifted[0][1]
+            print(new_chrid, new_chrpos)
+            matched += 1
+            db.gwas.update_many({'CHR_ID': chrid, 'CHR_POS': chrpos}, {'$set': {'hg19chr': new_chrid, 'hg19pos': new_chrpos}})
+        else:
+            #print('NONE: %s %s' %(chrid, chrpos))
+            unmatched += 1
+
+print(unmatched, matched)
