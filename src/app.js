8c788634252282c15a1340c3f3bc822220926019
Sigurd Gartmann <sigurd.gartmann@ntnu.no>
2016-09-30 17:51:34 +0200

    Remove traces of the old communications layer.
    Still needs to create new export functions.

--- src/app.js
+++ src/app.js
@@ -1,17 +1,12 @@
 import path from 'path';
 import express from 'express';
-import csv from 'fast-csv';
+// import csv from 'fast-csv';
 import favicon from 'serve-favicon';
-import ip from 'ip';
 import ReactDOMServer from 'react-dom/server';
-import Result from './models/Result';
-import Trait from './models/Trait';
 import Request from './models/Request';
 import db from './lib/db';
 import routes from './routes';
-import { map, startsWith, endsWith, each, values } from 'lodash';
 import bodyParser from 'body-parser';
-import moment from 'moment';
 import Router from 'isomorphic-relay-router';
 import RelayLocalSchema from 'relay-local-schema';
 // import Helmet from 'react-helmet';
@@ -50,13 +45,6 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/traits', (req, res, next) => {
-    Trait.find().sort('_id').exec((err, traits) => {
-        if (err) { return next(err); }
-        res.json({ traits });
-    });
-});
-
 app.post('/variables/:trait', (req, res, next) => {
     let rsids;
     if (Array.isArray(req.body.rsids)) {
@@ -76,188 +64,6 @@ app.post('/variables/:trait', (req, res, next) => {
     res.end();
 });
 
-app.get('/requests', (req, res, next) => {
-    Request.count().exec((err, totalRequests) => {
-        if (err) { return next(err); }
-        const localStart = ip.toBuffer('129.241.0.0');
-        const localEnd = ip.toBuffer('129.241.255.255');
-        Request.count({
-            $and: [
-                { remote_address: { $gte: localStart } },
-                { remote_address: { $lte: localEnd } },
-            ],
-        }).exec((err, localRequests) => {
-            const requests = {
-                total: totalRequests,
-                local: localRequests,
-            };
-            if (err) { return next(err); }
-            res.format({
-                html: () => {
-                    res.locals.data = { GwasStore: {
-                        requests,
-                    } };
-                    next();
-                },
-                json: () => {
-                    res.json({
-                        requests,
-                    });
-                },
-            });
-        });
-    });
-});
-
-app.get('_/search/', (req, res, next) => {
-    let download = false;
-    const query = {};
-    const fields = [];
-
-    // query param handling
-
-    // filter on values known in tromso
-    const tromso = JSON.parse(req.query.tromso || 'false');
-    const unique = JSON.parse(req.query.unique || 'false');
-
-    let q = req.query.q;
-    if (q && q.length < 3) {
-        q = '';
-    }
-    if (q) {
-        if (endsWith(q, '.csv')) {
-            q = q.replace(/\.csv$/, '');
-            download = true;
-        }
-        // TODO: Should we fix the SNP field to be an integer? YES, it's not
-        // even working normally now, but that may be a different issue.
-        const chrSearch = q.match(/^chr(\w+):(\d+)$/);
-        if (!isNaN(q)) {
-            fields.push({ PUBMEDID: q });
-            fields.push({ SNPS: 'rs' + q });
-        }
-        else if (chrSearch) {
-            const [_, chrid, chrpos] = chrSearch;
-            query.CHR_ID = +chrid;
-            query.CHR_POS = +chrpos;
-        }
-        else if (startsWith(q, 'rs')) {
-            /*
-            q = q.replace("rs", "");
-            if (!isNaN(q)) {
-                fields.push({SNP_ID_CURRENT: q});
-            }
-            */
-            fields.push({ SNPS: q });
-        }
-        else {
-            const r = RegExp(q, 'i');
-            fields.push({ REGION: { $regex: r } });
-            fields.push({ 'FIRST AUTHOR': { $regex: r } });
-            fields.push({ traits: { $regex: r } });
-            fields.push({ 'DISEASE/TRAIT': { $regex: r } });
-            fields.push({ 'MAPPED_GENE': { $regex: r } });
-        }
-    }
-    if (fields.length) {
-        query.$or = fields;
-    }
-    if (tromso) {
-        query.imputed = { $exists: 1 };
-    }
-    query['P-VALUE'] = { $lt: 0.00000005, $exists: 1, $ne: null };
-
-    Result.aggregate({ $match: query }, { $group: { _id: '$SNP_ID_CURRENT', count: { $sum: 1 } } }).exec((err, count) => {
-        if (err) { return next(err); }
-
-        const different = count.length;
-        const total = count.reduce((previous, current) => {
-            return previous + current.count;
-        }, 0);
-
-        if (q) {
-            Result.find(query).limit(1000).sort('CHR_ID CHR_POS').lean().exec((err, results) => {
-                if (err) { return next(err); }
-                if (unique) {
-                    const rsids = {};
-                    each(results, result => {
-                        const rsid = result.SNP_ID_CURRENT;
-                        if (rsids[rsid]) {
-                            const oldres = rsids[rsid];
-
-                            const oldP = oldres['P-VALUE'];
-                            const newP = result['P-VALUE'];
-
-                            if (!newP) {
-                                return;
-                            }
-
-                            const oldDate = moment(oldres.DATE, 'DD-MMM-YYYY');
-                            const newDate = moment(result.DATE, 'DD-MMM-YYYY');
-
-                            if (newDate.isBefore(oldDate)) {
-                                return;
-                            }
-
-                            if (newDate.isSame(oldDate)) {
-                                if (newP > oldP) {
-                                    return;
-                                }
-                            }
-                        }
-                        rsids[rsid] = result;
-                    });
-
-                    results = values(rsids); // eslint-disable-line no-param-reassign
-                }
-                if (download) {
-                    results = map(results, (result) => {
-                        result.SNP_ID_CURRENT = `rs${result.SNP_ID_CURRENT}`;
-                        result['STRONGEST SNP-RISK ALLELE'] = result['STRONGEST SNP-RISK ALLELE'].split('-').pop();
-                        return result;
-                    });
-                    csv.writeToString(results, { headers: ['SNP_ID_CURRENT', 'CHR_ID', 'CHR_POS', 'STRONGEST SNP-RISK ALLELE', 'P-VALUE', 'OR or BETA', '95% CI (TEXT)', 'RISK ALLELE FREQUENCY', 'hg19chr', 'hg19pos', 'MAPPED_GENE', 'INITIAL SAMPLE DESCRIPTION', 'REPLICATION SAMPLE DESCRIPTION', 'DATE'], delimiter: '\t' }, (err, data) => {
-                        res.set('Content-Type', 'text/csv');
-                        res.set('Content-Disposition', `attachment; filename=export-${q.replace(/[^a-zA-Z0-9]/g, '-')}.csv`);
-                        res.write(data);
-                        res.end();
-                    });
-                }
-                else {
-                    res.format({
-                        html: () => {
-                            res.locals.data = { GwasStore: {
-                                results: { different, total, data: results },
-                            } };
-                            next();
-                        },
-                        json: () => {
-                            res.json({
-                                results: { different, total, data: results },
-                            });
-                        },
-                    });
-                }
-            });
-        }
-        else {
-            res.format({
-                html: () => {
-                    res.locals.data = { GwasStore: {
-                        results: { different, total, data: [] },
-                    } };
-                    next();
-                },
-                json: () => {
-                    res.json({
-                        results: { different, total, data: [] },
-                    });
-                },
-            });
-        }
-    });
-});
-
 if (app.settings.env === 'production') {
     app.use(favicon(__dirname + '/../dist/favicon.ico'));
 }
@@ -265,32 +71,6 @@ else {
     app.use(favicon(__dirname + '/../src/favicon.ico'));
 }
 app.use(express.static(path.join(__dirname, '..', 'dist')));
-
-/*
-app.use((req, res) => {
-    alt.bootstrap(JSON.stringify(res.locals.data || {}));
-    match({ routes, location: req.url }, (err, redirectLocation, renderProps) => {
-        if (err) {
-            res.status(500).send(err.message);
-        }
-        else if (redirectLocation) {
-            res.redirect(302, redirectLocation.pathname + redirectLocation.search);
-        }
-        else if (renderProps) {
-            const iso = new Iso();
-            const content = ReactDOMServer.renderToString(<RoutingContext {...renderProps} />);
-            iso.add(content, alt.flush());
-
-            const markup = `<!doctype html><html><head><meta charset="utf-8" /><title>GWASC</title><meta name="viewport" content="width=device-width, initial-scale=1.0" /><link href="/stylesheet.css" rel="stylesheet" /></head><body>${iso.render(content)}<script src="/client.js"></script></body></html>`;
-            res.write(markup);
-            res.end();
-        }
-        else {
-            res.status(404).send('Not found');
-        }
-    });
-});
-*/
 
 app.use('/graphql', graphqlHTTP(req => ({
     schema,
