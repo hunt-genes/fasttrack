34861a7ab0f4e93800d3467240abb9c2aef752ba
Sigurd Gartmann <sigurd.gartmann@ntnu.no>
2016-10-11 14:44:38 +0200

    This is the old version of lifting, for new database
    Warning: Subtracting 1 should lead to adding 1 elsewhere

--- lift_db.py
+++ lift_db.py
@@ -2,24 +2,29 @@ from pymongo import MongoClient
 from pyliftover import LiftOver
 
 mongo_client = MongoClient()
-db = mongo_client.gwasc
+db = mongo_client.fasttrack
 
 lo = LiftOver('hg38ToHg19.over.chain.gz')
 
 unmatched = 0
 matched = 0
 for r in db.gwas.find():
-    chrid = r['CHR_ID']
-    chrpos = r['CHR_POS']
+    chrid = r['chr_id']
+    chrpos = r['chr_pos']
     if chrid and chrpos:
-        lifted = lo.convert_coordinate('chr%s' % chrid, chrpos - 1)
-        if lifted:
-            new_chrid = lifted[0][0].split('chr')[1]
-            new_chrpos = lifted[0][1]
-            matched += 1
-            db.gwas.update_many({'CHR_ID': chrid, 'CHR_POS': chrpos}, {'$set': {'hg19chr': new_chrid, 'hg19pos': new_chrpos}})
+        try:
+            _chrpos = int(chrpos)
+        except:
+            pass
         else:
-            #print('NONE: %s %s' %(chrid, chrpos))
-            unmatched += 1
+            lifted = lo.convert_coordinate('chr%s' % chrid, _chrpos - 1)
+            if lifted:
+                new_chrid = lifted[0][0].split('chr')[1]
+                new_chrpos = lifted[0][1]
+                matched += 1
+                db.gwas.update_many({'chr_id': chrid, 'chr_pos': chrpos}, {'$set': {'hg19chr': new_chrid, 'hg19pos': new_chrpos}})
+            else:
+                #print('NONE: %s %s' %(chrid, chrpos))
+                unmatched += 1
 
 print(unmatched, matched)
