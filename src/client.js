84e3454422f176e27fb0039ff679bfcf00b5884b
Sigurd Gartmann <sigurd.gartmann@ntnu.no>
2017-02-22 20:09:04 +0100

    Support queries in created browserHistory

--- src/client.js
+++ src/client.js
@@ -9,14 +9,14 @@ import IsomorphicRelay from 'isomorphic-relay';
 import IsomorphicRouter from 'isomorphic-relay-router';
 import { match, Router } from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import { createHistory } from 'history';
+import { createHistory, useQueries } from 'history';
 import moment from 'moment';
 import prefix from './prefix';
 import routes from './routes';
 import './scss/stylesheet.scss';
 
 injectTapEventPlugin();
-const browserHistory = createHistory();
+const browserHistory = useQueries(createHistory)();
 
 const environment = new Relay.Environment();
 environment.injectNetworkLayer(new Relay.DefaultNetworkLayer(`${prefix}/graphql`, {
