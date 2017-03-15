8164fecb8028c4c8eda2c4ac30b00b16ece6314a
Per Bjarne Løvsletten <per.b.lovsletten@ntnu.no>
2017-03-15 10:44:16 +0100

    Fixed a non working URL in the footer-component.
    Now pointing to the top level page at the GWAS catalog instead of a non
    working link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ export default class Footer extends React.Component {
                 <div style={{ maxWidth: 800, margin: '0 auto' }}>
                     <footer style={{ fontSize: 11, color: '#aaa', textAlign: 'center', paddingBottom: 50, margin: '0 10px' }}>
                         <Link to="about">About</Link> this service.<br />
-                        GWAS data from <ExternalLink href="https://www.ebi.ac.uk/gwas/docs/downloads">NHGRI-EBI</ExternalLink>, downloaded 2016-10-12.<br />
+                        GWAS data from <ExternalLink href="https://www.ebi.ac.uk/gwas/">NHGRI-EBI</ExternalLink>, downloaded 2016-10-12.<br />
                         The usual warnings about providing the service AS-IS applies.<br />
                         <ExternalLink href="https://www.ntnu.edu/huntgenes">K.G. Jebsen Center for Genetic Epidemiology</ExternalLink>, <ExternalLink href="http://www.ntnu.edu/ism">Department of public health and general practice (ISM)</ExternalLink>, <ExternalLink href="http://www.ntnu.edu/">Norwegian university of science and technology (NTNU)</ExternalLink>
                         <br />
